94d8ac0e90dc1cab3a963bee61033fcf30825c02
VaibhavGoyal9805 <govaibhav088@gmail.com>
2024-08-27 22:10:26 +0530

    Update basicgame.py

--- basicgame.py
+++ basicgame.py
@@ -1,1 +1,33 @@
+import random
+computer = random.choice([-1,1,0])
+youstr = input("enter your choice :")
+youDict = {'s':1 , 'w':-1, 'g' :0}
+reverseDict = { 1: "snake", -1 : "water" , 0 : "gun"}
+you = youDict[youstr]
+
+
+print (f"you choose {reverseDict[you]}\n Computer choose{reverseDict[computer]}")
+if(computer == you):
+    print("it's a draw ! please try again")
+else:
+    if(computer == -1 and you == 1):
+        print("hurray! you win")
+
+    elif(computer == -1 and you == 0):
+         print("alas! you lost")
+    
+    elif(computer == 1 and you == -1):
+        print("Alas! you lost")
+
+    elif(computer == 1 and you == 0):
+        print("hurray! you win")
+
+    elif(computer == 0 and you == 1):
+        print("Alas! you lost")
+
+    elif(computer == 0 and you == -1):
+        print("hurray! you win")
+
+    else:
+        print("something went wrong!")
 
